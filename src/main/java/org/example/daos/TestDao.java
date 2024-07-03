909f0faee12a2455901811db01711b10648249b6
Joel Anderson <joel.anderson@kainos.com>
2024-07-03 14:47:26 +0100

    Adding logging to database list

--- src/main/java/org/example/daos/TestDao.java
+++ src/main/java/org/example/daos/TestDao.java
@@ -22,9 +22,13 @@ public class TestDao {
             long end = System.currentTimeMillis();
 
             System.out.println("Total time to execute query in milliseconds: " + (end - start));
+
+            System.out.println("Retrieving database names");
             while (resultSet.next()) {
+                System.out.println(resultSet.getString("Database"));
                 databases.add(resultSet.getString("Database"));
             }
+            System.out.println("Finished retrieving database names");
         }
         return databases;
     }
